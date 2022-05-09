a9622357051e8c297ed3b62faf87f953e71b425e
he ping <847050412@qq.com>
2022-05-10 00:36:01 +0800

    xxx

--- src/components/registerGlobComp.ts
+++ src/components/registerGlobComp.ts
@@ -3,8 +3,40 @@ import { Button } from './Button';
 import {
   // Need
   Button as AntButton,
+  Select,
+  Alert,
+  Checkbox,
+  DatePicker,
+  Radio,
+  Switch,
+  Card,
+  List,
+  Tabs,
+  Descriptions,
+  Tree,
+  Table,
+  Divider,
+  Modal,
+  Drawer,
+  Dropdown,
+  Tag,
+  Tooltip,
+  Badge,
+  Popover,
+  Upload,
+  Transfer,
+  Steps,
+  PageHeader,
+  Result,
+  Empty,
+  Avatar,
+  Menu,
+  Breadcrumb,
+  Form,
   Input,
-  Layout,
+  Row,
+  Col,
+  Spin,
 } from 'ant-design-vue';
 
 const compList = [AntButton.Group];
@@ -14,5 +46,39 @@ export function registerGlobComp(app: App) {
     app.component(comp.name || comp.displayName, comp);
   });
 
-  app.use(Input).use(Button).use(Layout);
+  app.use(Select)
+      .use(Alert)
+      .use(Button)
+      .use(Breadcrumb)
+      .use(Checkbox)
+      .use(DatePicker)
+      .use(Radio)
+      .use(Switch)
+      .use(Card)
+      .use(List)
+      .use(Descriptions)
+      .use(Tree)
+      .use(Table)
+      .use(Divider)
+      .use(Modal)
+      .use(Drawer)
+      .use(Dropdown)
+      .use(Tag)
+      .use(Tooltip)
+      .use(Badge)
+      .use(Popover)
+      .use(Upload)
+      .use(Transfer)
+      .use(Steps)
+      .use(PageHeader)
+      .use(Result)
+      .use(Empty)
+      .use(Avatar)
+      .use(Menu)
+      .use(Tabs)
+      .use(Form)
+      .use(Input)
+      .use(Row)
+      .use(Col)
+      .use(Spin);
 }
--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -1,69 +1,107 @@
 <template>
   <PageWrapper title="聊天">
-    <ChatWindow
-      :current-user-id="currentUserId"
-      :rooms="rooms"
-      :messages="messages"
-      :rooms-loaded="true"
-      :messages-loaded="true"
-    />
+    <div class="flex">
+      <div class="w-1/3 bg-white p-4">
+        <div class="flex items-stretch">
+          <div>
+            <a-input-search
+                v-model:value="value"
+                placeholder="搜索好友或群组"
+                style="width: 200px;border-radius:25px"
+                @search="onSearch"
+            />
+          </div>
+
+          <div>
+            <UserAddOutlined :style="{fontSize: '31px', color: 'green', margin: '0px 0px 0px 15px'}"/>
+          </div>
+        </div>
+        <div class="flex">
+        </div>
+        <p class="text-xs font-medium mt-4">消息列表({{ nums }})</p>
+        <div class="flex">
+          <template></template>
+          <a-list
+              class="demo-loadmore-list"
+              :loading="loading"
+              item-layout="horizontal"
+              :data-source="dataList"
+          >
+            <template #loadMore>
+              <div :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
+                <a-spin v-if="loadingMore"/>
+                <a-button v-else @click="loadMore">loading more</a-button>
+              </div>
+            </template>
+            <template #renderItem="{ item }">
+              <a-list-item>
+
+                <a-list-item-meta
+                >
+                  <template #title>
+                    <a href="https://www.antdv.com/">{{ item.name.last }}</a>
+                  </template>
+                  <template #avatar>
+                    <a-avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
+                  </template>
+                </a-list-item-meta>
+                <div>content</div>
+              </a-list-item>
+            </template>
+          </a-list>
+        </div>
+      </div>
+
+      <div class="w-2/3 bg-white ml-4 p-4">
+        <span class="text-lg font-medium mr-4"> 消息记录: </span>
+        <hr class="my-4"/>
+
+        <div class="max-h-80 overflow-auto">
+          <ul>
+          </ul>
+        </div>
+      </div>
+    </div>
   </PageWrapper>
 </template>
-<script>
-  import { PageWrapper } from '/@/components/Page';
-  import { ChatWindow } from 'vue-advanced-chat';
-  import 'vue-advanced-chat/dist/vue-advanced-chat.css';
+<script lang="ts">
+import {PageWrapper} from '/@/components/Page';
+import {defineComponent, ref} from 'vue';
+import {UserAddOutlined} from '@ant-design/icons-vue';
+import {useLoadMore} from 'vue-request';
 
-  export default {
-    components: {
-      ChatWindow,
-      PageWrapper,
-    },
-    data() {
-      return {
-        rooms: [
-          {
-            roomId: 1,
-            roomName: 'Room 1',
-            avatar: 'assets/imgs/people.png',
-            unreadCount: 4,
-            index: 3,
-            lastMessage: {
-              content: 'Last message received',
-              senderId: 1234,
-              username: 'John Doe',
-              timestamp: '10:20',
-              saved: true,
-              distributed: false,
-              seen: false,
-              new: true,
-            },
-            users: [
-              {
-                _id: 1234,
-                username: 'John Doe',
-                avatar: 'assets/imgs/doe.png',
-                status: {
-                  state: 'online',
-                  lastChanged: 'today, 14:30',
-                },
-              },
-              {
-                _id: 4321,
-                username: 'John Snow',
-                avatar: 'assets/imgs/snow.png',
-                status: {
-                  state: 'offline',
-                  lastChanged: '14 July, 20:00',
-                },
-              },
-            ],
-            typingUsers: [4321],
-          },
-        ],
-        messages: [],
-        currentUserId: 1234,
-      };
-    },
-  };
+const getFakeData = () => `https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo`;
+
+export default defineComponent({
+  components: {
+    PageWrapper,
+    UserAddOutlined,
+    useLoadMore
+  },
+  setup() {
+    const value = ref<string>('');
+    const nums = 1;
+    const onSearch = (searchValue: string) => {
+      console.log('use value', searchValue);
+      console.log('or use this.value', value.value);
+    };
+    const {dataList, loading, loadingMore, loadMore} = useLoadMore(getFakeData, {
+      listKey: 'results',
+    });
+    return {
+      value,
+      onSearch,
+      nums,
+      loading,
+      loadingMore,
+      dataList,
+      loadMore,
+    };
+  },
+});
 </script>
+<style scoped>
+.demo-loadmore-list{
+  min-height:350px;
+}
+</style>
